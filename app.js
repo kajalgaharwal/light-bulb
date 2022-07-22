8e907df68ab7a931d367b094e4e082dcc7fb6dea
Kajal Gaharwal <55242053+kajalgaharwal@users.noreply.github.com>
2022-07-22 22:32:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+let btn = document.getElementById('btn');
+const offBulb = document.getElementById('offBulb');
+const onBulb = document.getElementById('onBulb');
+let container = document.querySelector('.container');
+let imgs = document.createElement('img');
+imgs.src = '/bulb_light.jpg';
+imgs.id = 'onBulb';
+imgs.className = 'active';
+
+btn.addEventListener('click', () => {
+  if (btn.innerText === 'ON') {
+    container.appendChild(imgs);
+    btn.innerText = 'OFF';
+  } else {
+    container.removeChild(imgs);
+    btn.innerText = 'ON';
+  }
+});
